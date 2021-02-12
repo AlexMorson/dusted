e5ebd47ac82f55066223978fe0cca61009499a88
Alex Morson <alexmorson@btinternet.com>
2021-02-12 10:54:57 +0000

    Move LevelView class to new file

--- gui.py
+++ gui.py
@@ -1,143 +1,23 @@
-import math
 import os
 import queue
 import re
 import tkinter as tk
 import tkinter.filedialog
 
-from cursor import Cursor
 import dustforce
-import geom
+import utils
+from cursor import Cursor
+from dialog import Dialog
 from inputs import Inputs
 from inputs_view import InputsView
-import level
-from dialog import Dialog
+from level import Level
+from level_view import LevelView
 from replay import Replay
-from utils import *
 
 LEVEL_PATTERN = r"START (.*)"
 COORD_PATTERN = r"(\d*) (-?\d*) (-?\d*)"
 CHARACTERS = ["dustman", "dustgirl", "dustworth", "dustkid"]
 
-
-class LevelView(tk.Canvas):
-    def __init__(self, parent, level, cursor):
-        super().__init__(parent, height=0)
-
-        self.level = level
-        self.level.subscribe(self.on_level_change)
-        self.cursor = cursor
-        self.cursor.subscribe(self.on_cursor_move)
-
-        self.bind("<Button-4>", self.on_scroll) # Linux
-        self.bind("<Button-5>", self.on_scroll)
-        self.bind("<MouseWheel>", self.on_scroll) # Windows
-        self.bind("<Button-1>", self.on_click)
-        self.bind("<B1-Motion>", self.on_drag)
-        self.bind("<Button-3>", self.on_right_click)
-
-        self.reset()
-
-    def reset(self):
-        self.zoom_level = 1
-        self.offset_x = self.offset_y = 0
-        self.prev_mx = self.prev_my = 0
-        self.coords = []
-        self.path_objects = []
-        self.position_object = None
-        self.delete("all")
-
-    def on_level_change(self):
-        self.reset()
-
-        level_data = load_level_from_id(self.level.get())
-        tiles = {(x, y) for (l, x, y), t in level_data.tiles.items() if l == 19}
-        outlines = geom.tile_outlines(tiles)
-        for outline in outlines:
-            self.create_polygon(*[(48*x, 48*y) for x, y in outline[0]], fill="#bbb")
-            for hole in outline[1:]:
-                self.create_polygon(*[(48*x, 48*y) for x, y in hole], fill="#d9d9d9")
-
-    def select_frame(self, frame):
-        if self.position_object is not None: self.delete(self.position_object)
-        if 0 <= frame < len(self.coords):
-            x, y = self.coords[frame]
-            self.position_object = self.create_rectangle(x-24, y-48, x+24, y+48)
-            self.fix_object(self.position_object)
-        else:
-            self.position_object = None
-
-    def add_coordinate(self, frame, x, y):
-        if frame < len(self.coords): # Clear suffix
-            for i in self.path_objects[max(0, frame-1):]:
-                self.delete(i)
-            self.path_objects = self.path_objects[:max(0, frame-1)]
-            self.coords = self.coords[:frame]
-        elif frame > len(self.coords): # Loaded state in the future, pad values
-            self.path_objects.extend([-1] * (frame - min(1, len(self.coords)) + 1))
-            self.coords.extend([(x, y)] * (frame - len(self.coords) + 1))
-            return
-
-        self.coords.append((x, y))
-        if frame > 0:
-            i = self.create_line(*self.coords[frame-1], *self.coords[frame])
-            self.fix_object(i)
-            self.path_objects.append(i)
-
-    def fix_object(self, i):
-        self.scale(i, 0, 0, self.zoom_level, self.zoom_level)
-        self.move(i, self.offset_x, self.offset_y)
-
-    def zoom(self, x, y, scale):
-        self.zoom_level *= scale
-        self.offset_x = (self.offset_x - x) * scale + x;
-        self.offset_y = (self.offset_y - y) * scale + y;
-        self.scale("all", x, y, scale, scale)
-
-    def pan(self, dx, dy):
-        self.offset_x += dx
-        self.offset_y += dy
-        self.move("all", dx, dy)
-
-    def on_cursor_move(self):
-        _, col = self.cursor.position()
-        self.select_frame(col)
-
-    def on_scroll(self, event):
-        if event.num == 4 or event.delta == 120:
-            scale = 1.25
-        if event.num == 5 or event.delta == -120:
-            scale = 0.8
-        self.zoom(event.x, event.y, scale)
-
-    def on_click(self, event):
-        self.prev_mx = event.x
-        self.prev_my = event.y
-
-    def on_drag(self, event):
-        dx = event.x - self.prev_mx
-        dy = event.y - self.prev_my
-
-        self.pan(dx, dy)
-
-        self.prev_mx = event.x
-        self.prev_my = event.y
-
-    def on_right_click(self, event):
-        cx = (event.x - self.offset_x) / self.zoom_level
-        cy = (event.y - self.offset_y) / self.zoom_level
-
-        closest = None
-        dist = 1e10
-        for i, (x, y) in enumerate(self.coords):
-            d = math.hypot(cx-x, cy-y)
-            if d < dist:
-                dist = d
-                closest = i
-
-        if closest is not None:
-            row, _ = self.cursor.position()
-            self.cursor.set(row, closest)
 
 
 class ReplayDialog(Dialog):
@@ -146,7 +26,7 @@ class ReplayDialog(Dialog):
         self.app = app
 
     def ok(self, replay_id):
-        replay = load_replay_from_id(replay_id)
+        replay = utils.load_replay_from_id(replay_id)
         self.app.load_replay(replay)
         return True
 
@@ -197,7 +77,7 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.level = level.Level()
+        self.level = Level()
         self.character = 0
         self.inputs = Inputs()
         self.cursor = Cursor(self.inputs)
@@ -264,7 +144,7 @@ class App(tk.Tk):
             if not self.file:
                 return False
         replay = Replay("TAS", self.level.get(), self.character, self.inputs.get())
-        write_replay_to_file(self.file, replay)
+        utils.write_replay_to_file(self.file, replay)
         return True
 
     def new_file(self):
@@ -277,7 +157,7 @@ class App(tk.Tk):
             title="Load replay"
         )
         if filepath:
-            replay = load_replay_from_file(filepath)
+            replay = utils.load_replay_from_file(filepath)
             self.load_replay(replay, filepath)
 
     def load_replay(self, replay, filepath=None):
---
+++ level_view.py
@@ -0,0 +1,125 @@
+import math
+
+import tkinter as tk
+
+import geom
+from utils import load_level_from_id
+
+class LevelView(tk.Canvas):
+    def __init__(self, parent, level, cursor):
+        super().__init__(parent, height=0)
+
+        self.level = level
+        self.level.subscribe(self.on_level_change)
+        self.cursor = cursor
+        self.cursor.subscribe(self.on_cursor_move)
+
+        self.bind("<Button-4>", self.on_scroll) # Linux
+        self.bind("<Button-5>", self.on_scroll)
+        self.bind("<MouseWheel>", self.on_scroll) # Windows
+        self.bind("<Button-1>", self.on_click)
+        self.bind("<B1-Motion>", self.on_drag)
+        self.bind("<Button-3>", self.on_right_click)
+
+        self.reset()
+
+    def reset(self):
+        self.zoom_level = 1
+        self.offset_x = self.offset_y = 0
+        self.prev_mx = self.prev_my = 0
+        self.coords = []
+        self.path_objects = []
+        self.position_object = None
+        self.delete("all")
+
+    def on_level_change(self):
+        self.reset()
+
+        level_data = load_level_from_id(self.level.get())
+        tiles = {(x, y) for (l, x, y), t in level_data.tiles.items() if l == 19}
+        outlines = geom.tile_outlines(tiles)
+        for outline in outlines:
+            self.create_polygon(*[(48*x, 48*y) for x, y in outline[0]], fill="#bbb")
+            for hole in outline[1:]:
+                self.create_polygon(*[(48*x, 48*y) for x, y in hole], fill="#d9d9d9")
+
+    def select_frame(self, frame):
+        if self.position_object is not None: self.delete(self.position_object)
+        if 0 <= frame < len(self.coords):
+            x, y = self.coords[frame]
+            self.position_object = self.create_rectangle(x-24, y-48, x+24, y+48)
+            self.fix_object(self.position_object)
+        else:
+            self.position_object = None
+
+    def add_coordinate(self, frame, x, y):
+        if frame < len(self.coords): # Clear suffix
+            for i in self.path_objects[max(0, frame-1):]:
+                self.delete(i)
+            self.path_objects = self.path_objects[:max(0, frame-1)]
+            self.coords = self.coords[:frame]
+        elif frame > len(self.coords): # Loaded state in the future, pad values
+            self.path_objects.extend([-1] * (frame - min(1, len(self.coords)) + 1))
+            self.coords.extend([(x, y)] * (frame - len(self.coords) + 1))
+            return
+
+        self.coords.append((x, y))
+        if frame > 0:
+            i = self.create_line(*self.coords[frame-1], *self.coords[frame])
+            self.fix_object(i)
+            self.path_objects.append(i)
+
+    def fix_object(self, i):
+        self.scale(i, 0, 0, self.zoom_level, self.zoom_level)
+        self.move(i, self.offset_x, self.offset_y)
+
+    def zoom(self, x, y, scale):
+        self.zoom_level *= scale
+        self.offset_x = (self.offset_x - x) * scale + x;
+        self.offset_y = (self.offset_y - y) * scale + y;
+        self.scale("all", x, y, scale, scale)
+
+    def pan(self, dx, dy):
+        self.offset_x += dx
+        self.offset_y += dy
+        self.move("all", dx, dy)
+
+    def on_cursor_move(self):
+        _, col = self.cursor.position()
+        self.select_frame(col)
+
+    def on_scroll(self, event):
+        if event.num == 4 or event.delta == 120:
+            scale = 1.25
+        if event.num == 5 or event.delta == -120:
+            scale = 0.8
+        self.zoom(event.x, event.y, scale)
+
+    def on_click(self, event):
+        self.prev_mx = event.x
+        self.prev_my = event.y
+
+    def on_drag(self, event):
+        dx = event.x - self.prev_mx
+        dy = event.y - self.prev_my
+
+        self.pan(dx, dy)
+
+        self.prev_mx = event.x
+        self.prev_my = event.y
+
+    def on_right_click(self, event):
+        cx = (event.x - self.offset_x) / self.zoom_level
+        cy = (event.y - self.offset_y) / self.zoom_level
+
+        closest = None
+        dist = 1e10
+        for i, (x, y) in enumerate(self.coords):
+            d = math.hypot(cx-x, cy-y)
+            if d < dist:
+                dist = d
+                closest = i
+
+        if closest is not None:
+            row, _ = self.cursor.position()
+            self.cursor.set(row, closest)
